981cbcf3d33dc2f2b2d33c1f5e6124dc9fa14c3f
ryparker <ryan.parker3@outlook.com>
2021-03-11 08:45:05 -0600

    Fix(ActionStepper) made content text white for readability

--- src/components/ActionsStepper/index.tsx
+++ src/components/ActionsStepper/index.tsx
@@ -130,7 +130,6 @@ const StepContent = styled.div<{ minimized?: boolean }>`
   border-radius: 4px;
 
   font-weight: 500;
-  color: var(--color-gray300);
 
   ${(props) => {
     if (props.minimized) {
